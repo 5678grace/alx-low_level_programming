a7100fd60b4bfb65d693c3f3e38df851361e50e7
5678grace <minoograce@gmail.com>
2023-06-20 06:21:12 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * main - print lowercase alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n')
+}
